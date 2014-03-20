d13de0433de44ab77ff0e3eaeeb8e41dcee93652
Nate Watson <meromorphic@gmail.com>
2014-03-20 13:23:38 -0700

    removed collections from tree

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,7 +19,6 @@
 //= require_tree ./board
 //= require_tree ../templates
 //= require_tree ./models
-//= require_tree ./collections
 //= require_tree ./views
 //= require_tree ./routers
 //= require_tree .
